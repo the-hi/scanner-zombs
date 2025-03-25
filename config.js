fd997e13562e1f9613cf145f405fbe1e65982c6a
the_hi. <mahadevmanoj4@gmail.com>
2025-03-25 18:31:19 +0400

    new commands

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // UPDATE THIS PLSSS
 const config = {
-    TOKEN: "MTM1NDAxMDQ2MzAwNzkzNjU3Mw.GKhOmH.tiWRph9bw4kp3nVt00UEGegSYdaqkdcd3yHVSk",
+    TOKEN: "",
     CLIENT_ID: "1354010463007936573"
 }
 export { config };
