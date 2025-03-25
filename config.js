cb5683e958d264603437ae3707abf471f004babc
the_hi. <mahadevmanoj4@gmail.com>
2025-03-25 14:26:22 +0400

    sum stuf

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // UPDATE THIS PLSSS
 const config = {
-    TOKEN: "MTM1NDAxMDQ2MzAwNzkzNjU3Mw.GtAIcZ.vbPxkA_x0AILCEg-ZW6rIYEND1BKh4huykSq6k",
+    TOKEN: "",
     CLIENT_ID: "1354010463007936573"
 }
 export { config };
