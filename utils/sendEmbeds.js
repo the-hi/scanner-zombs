6e77a04f074923f21b90a94506b8de01a9857719
the_hi. <mahadevmanoj4@gmail.com>
2025-04-07 21:24:25 +0400

    batman

--- utils/sendEmbeds.js
+++ utils/sendEmbeds.js
@@ -14,67 +14,59 @@ const sendEmbeds = async (interaction, embeds) => {
             .setDisabled(isDisabled));
     })
     const row = new ActionRowBuilder().addComponents(buttons);
+    await interaction.editReply({ embeds: [embeds[0]], components: [row] });
     // defer reply
     await interaction.deferReply({ ephemeral: config.ephemeral });
     // update the embeds
-    const totalPages = embeds.length;
-    await interaction.editReply({ embeds: [embeds[0]], components: [row] });
-
     let currentPage = 0;
+    const totalPages = embeds.length;
     const msg = await interaction.fetchReply();
-    const collector = msg.createMessageComponentCollector({
-        time: 60000,
-        filter: (i) => i.user.id === interaction.user.id,
-    });
+    const collector = msg.createMessageComponentCollector({ time: 60000, filter: (i) => i.user.id === interaction.user.id });
 
     collector.on('collect', async (int) => {
-        try {
-            collector.resetTimer();
-            switch (int.customId) {
-                case '⏮️':
-                    currentPage = 0;
-                    break;
-                case '⏭️':
-                    currentPage = totalPages - 1;
-                    break;
-                case '⬅️':
-                    currentPage--;
-                    break;
-                case '➡️':
-                    currentPage++;
-                    break;
-            }
-            const buttons = [];
-            ['⏮️', '⬅️', '➡️', '⏭️'].forEach((emoji, index) => {
-                buttons.push(new ButtonBuilder()
-                    .setCustomId(emoji)
-                    .setStyle(ButtonStyle.Primary)
-                    .setEmoji(emoji)
-                    .setDisabled(index >= 2 ? currentPage === totalPages - 1 : currentPage === 0));
-            });
-            const newRow = new ActionRowBuilder().addComponents(buttons);
-            await int.update({ embeds: [embeds[currentPage]], components: [newRow] });
-        } catch (error) {
-            console.error('Error during collect:', error);
+        // reset timer
+        collector.resetTimer();
+        // handle interactions
+        switch (int.customId) {
+            case '⏮️':
+                currentPage = 0;
+                break;
+            case '⏭️':
+                currentPage = totalPages - 1;
+                break;
+            case '⬅️':
+                currentPage--;
+                break;
+            case '➡️':
+                currentPage++;
+                break;
         }
+        // create buttons
+        const buttons = [];
+        ['⏮️', '⬅️', '➡️', '⏭️'].forEach((emoji, index) => {
+            buttons.push(new ButtonBuilder()
+                .setCustomId(emoji)
+                .setStyle(ButtonStyle.Primary)
+                .setEmoji(emoji)
+                .setDisabled(index >= 2 ? currentPage === totalPages - 1 : currentPage === 0));
+        });
+        // update an interaction
+        const newRow = new ActionRowBuilder().addComponents(buttons);
+        await int.update({ embeds: [embeds[currentPage]], components: [newRow] });
     });
 
     collector.on('end', async () => {
-        try {
-            const buttons = [];
-            ['⏮️', '⬅️', '➡️', '⏭️'].forEach((emoji) => {
-                buttons.push(new ButtonBuilder()
-                    .setCustomId(emoji)
-                    .setStyle(ButtonStyle.Primary)
-                    .setEmoji(emoji)
-                    .setDisabled(true));
-            });
-            const disabledRow = new ActionRowBuilder().addComponents(buttons);
-            await interaction.editReply({ components: [disabledRow] });
-        } catch (error) {
-            console.error("Error on collector end: ", error);
-        }
-
+        // remove buttons
+        const buttons = [];
+        ['⏮️', '⬅️', '➡️', '⏭️'].forEach((emoji) => {
+            buttons.push(new ButtonBuilder()
+                .setCustomId(emoji)
+                .setStyle(ButtonStyle.Primary)
+                .setEmoji(emoji)
+                .setDisabled(true));
+        });
+        const disabledRow = new ActionRowBuilder().addComponents(buttons);
+        await interaction.editReply({ components: [disabledRow] });
     });
 };
 
